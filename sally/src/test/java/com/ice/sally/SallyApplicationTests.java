b4e5b0b3d136eb4c16dbf1552d7abf6d069444c8
Aditya Bhavaraju <adityateja.b@gmail.com>
2019-02-10 19:26:59 +0530

    initial skeleton

---
+++ sally/src/test/java/com/ice/sally/SallyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ice.sally;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SallyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
